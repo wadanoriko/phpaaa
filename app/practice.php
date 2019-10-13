1869a45c9824b483f11d93e99d73098243fee600
mac <mac@macnoMacBook-Air.local>
2019-10-13 19:14:40 +0900

    20191013

--- app/practice.php
+++ app/practice.php
@@ -1,7 +1,13 @@
 <?php
-$name = '鈴木';
-$name = '佐藤';
-echo $name.'さんこんにちは';
 
-$place = '北海道';
-echo $place;
+$str = 'こんにちは';
+$int = 8;
+$bool = TRUE;
+$float = 3.2;
+$null = NULL;
+
+var_dump($str);
+var_dump($int);
+var_dump($bool);
+var_dump($float);
+var_dump($null);
