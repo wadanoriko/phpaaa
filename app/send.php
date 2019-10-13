3a1058486763e74478c7faded8d141e7e6fc7289
mac <mac@macnoMacBook-Air.local>
2019-10-13 20:43:02 +0900

    20191013

---
+++ app/send.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h1>練習フォーム</h1>
+<p>次のページへデータを渡してみよう。</p>
+
+<form actions="./confirm.php" method="POST">
+<label>お名前</label>
+<input type="text" name="user_name">
+<label>趣味</label>
+<input type="text" name="hobby">
+<input type="submit" value="確認する">
+</form>
+
+
+
+
+</body>
+</html>
